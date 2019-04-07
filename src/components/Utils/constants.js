6a15dafd31167c062ba10941c5dbbdfb82c59968
Max <maxicespedes7@hotmail.com>
2019-04-07 11:26:39 +0200

    Improve description! longer and more descriptive

--- src/components/Utils/constants.js
+++ src/components/Utils/constants.js
@@ -1,1 +1,1 @@
-export const DEFAULT_DESCRIPTION_HEADER = `z3ro's thoughts, mainly javascript`
+export const DEFAULT_DESCRIPTION_HEADER = `z3ro's thoughts, mainly javascript, learn frontend together with me. Review libraries and frameworks! Witness the path to JS enlightment.`

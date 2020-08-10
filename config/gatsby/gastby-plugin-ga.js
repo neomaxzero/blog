9e6f58b193614c9c282063664817271930cf7339
Maxi <maximiliano.cespedes@coolblue.nl>
2020-08-10 14:46:04 +0200

    alopmst

--- config/gatsby/gastby-plugin-ga.js
+++ config/gatsby/gastby-plugin-ga.js
@@ -4,13 +4,5 @@ module.exports = {
     trackingId: 'UA-69994104-3',
     // Puts tracking script in the head instead of the body
     head: false,
-    // Setting this parameter is optional
-    // Setting this parameter is also optional
-    respectDNT: true,
-    // Avoids sending pageview hits from custom paths
-    exclude: ['/preview/**', '/do-not-track/me/too/'],
-    // Any additional create only fields (optional)
-    sampleRate: 5,
-    siteSpeedSampleRate: 10,
   },
 }

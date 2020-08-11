265819f605919f930a3d9808eb889ae3a82563f7
Maxi <maximiliano.cespedes@coolblue.nl>
2020-08-11 11:38:09 +0200

    best fallback font

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 const g_plugin_manifest = require('./config/gatsby/gatsby-plugin-manifest')
+const g_plugin_font_loader = require('./config/gatsby/gatsby-plugin-web-font-loader')
 const g_plugin_source_filesystem = require('./config/gatsby/gatsby-plugin-source-filesystem')
 const g_plugin_ga = require('./config/gatsby/gastby-plugin-ga')
 const g_plugin_prismjs = require('./config/gatsby/gatsby-plugin-prismjs')

c487fc06ddf178ba12343d99f6cd0e12d3d13eb2
Maxi <maximiliano.cespedes@coolblue.nl>
2020-09-19 12:23:00 +0200

    Add canonical to posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ const g_plugin_source_filesystem = require('./config/gatsby/gatsby-plugin-source
 const g_plugin_ga = require('./config/gatsby/gastby-plugin-ga')
 const g_plugin_prismjs = require('./config/gatsby/gatsby-plugin-prismjs')
 const g_plugin_sitemap = require('./config/gatsby/gatsby-plugin-sitemap')
+const g_plugin_canonicals = require('./config/gatsby/gatsby-plugin-canonical-urls')
 
 module.exports = {
   siteMetadata: {
@@ -23,5 +24,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     g_plugin_sitemap,
+    g_plugin_canonicals
   ],
 }
